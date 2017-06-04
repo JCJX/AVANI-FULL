32185aed1e911e1141689f04ff404558db9cc0a8
SlowlyEOF <zf19970822@outlook.com>
2017-06-04 16:06:45 +0800

    添加样式

--- theme-options.php
+++ theme-options.php
@@ -17,6 +17,57 @@
     {
 ?>
 
+<style>
+    .wrap, textarea, em {
+        font-family: 'Microsoft YaHei', Verdana;
+    }
+ 
+    fieldset {
+        width: 50%;
+        border: 1px solid #aaa;
+        padding-bottom: 20px;
+        margin-top: 20px;
+        -webkit-box-shadow: rgba(0, 0, 0, .2) 0px 0px 5px;
+        -moz-box-shadow: rgba(0, 0, 0, .2) 0px 0px 5px;
+        box-shadow: rgba(0, 0, 0, .2) 0px 0px 5px;
+       
+    }
+    fieldset  table  tr{
+         border-bottom: 1px  dashed  #dcde22;
+    }
+ 
+    legend {
+        margin-left: 5px;
+        padding: 0 5px;
+        color: #2481C6;
+        background: #F9F9F9;
+        cursor: pointer;
+    }
+ 
+    textarea {
+        width: 100%;
+        font-size: 11px;
+        border: 1px solid #aaa;
+        background: none;
+        -webkit-box-shadow: rgba(0, 0, 0, .2) 1px 1px 2px inset;
+        -moz-box-shadow: rgba(0, 0, 0, .2) 1px 1px 2px inset;
+        box-shadow: rgba(0, 0, 0, .2) 1px 1px 2px inset;
+        -webkit-transition: all .4s ease-out;
+        -moz-transition: all .4s ease-out;
+    }
+ 
+    textarea:focus {
+        -webkit-box-shadow: rgba(0, 0, 0, .2) 0px 0px 8px;
+        -moz-box-shadow: rgba(0, 0, 0, .2) 0px 0px 8px;
+        box-shadow: rgba(0, 0, 0, .2) 0px 0px 8px;
+        outline: none;
+    }
+    fieldset span{
+        color: #F0AD4E;
+    }
+ 
+</style>
+
 <div class="wrap">
     <h2>主题选项</h2> 
     <form method="post" action="">
