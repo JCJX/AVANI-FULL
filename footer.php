94b3a9ff5b4424652c6dcc3b2177d3b580c1b475
SlowlyEOF <zf19970822@outlook.com>
2017-07-12 03:02:03 +0900

    power clolor

--- footer.php
+++ footer.php
@@ -19,6 +19,12 @@
 	<?php avani_footer_widgets(); ?>
 	<footer id="colophon" class="site-footer">
         <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/<?php echo get_option('mytheme_colour'); ?>.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/js/activate-power-mode.js "></script>
+	<script>
+	POWERMODE.colorful = true; 
+	POWERMODE.shake = true; 
+	document.body.addEventListener('input', POWERMODE);
+	</script>
 		<div class="site-info">
 			<?php avani_footer_info(); ?>
 		</div><!-- .site-info -->
